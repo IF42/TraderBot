46be2424a0fa8caa43ec01e7ca0d0c100d65c9dc
Petr Horáček <phoracek.edit@gmail.com>
2023-06-16 23:52:38 +0200

    fixed indentation issues

--- TraderBot.py
+++ TraderBot.py
@@ -12,6 +12,7 @@ import numpy as np
 import json
 from getpass import getpass
 import os
+import enum
 import datetime
 
 
@@ -32,28 +33,27 @@ class Symbol:
     Container for historical data of given symbol with mathematical 
     analysis of the next step
     """
+    def __init__(self, name, history):
+        self.name    = name
+        self.history = history
+        self.status  = TradeStatus.NOTHING
 
-	def __init__(self, name, history):
-		self.name     = name
-		self.history  = history
-		self.status   = TradeStatus.NOTHING
+    def update_history(self, value):
+        self.history = self.history[1:]
+        self.history = np.append(self.history, value)
 
-	def update_history(self, value):
-		self.history = self.history[1:]
-		self.history = np.append(self.history, value)
+    def update_trade_status(self, status):
+        self.status   = status
 
-	def update_trade_status(self, status):
-		self.status   = status
+    def predict(self) -> Prediction:
+        t = talib.RSI(self.history, timeperiod = 7)[-1]
 
-	def predict(self) -> Prediction:
-		t = talib.RSI(self.history, timeperiod = 7)[-1]
-
-		if t > 70:
-		  return Prediction.BUY
-		elif t < 30:
-		  return Prediction.SELL
-		else:
-		  return Prediction.NOTHING
+        if t > 70:
+            return Prediction.BUY
+        elif t < 30:
+            return Prediction.SELL
+        else:
+            return Prediction.NOTHING
     
 
 class Trader:
@@ -64,70 +64,70 @@ class Trader:
 	TODO: initialize Symbol list with current open trades on trade server
     """
 
-	def __init__(self, user_id, password, mode, traded_symbols):
-		self.client = Client()
-		self.client.login(user_id, password, mode)
-		self.symbols = []
+    def __init__(self, user_id, password, mode, traded_symbols):
+        self.client = Client()
+        self.client.login(user_id, password, mode)
+        self.symbols = []
 
-		for symbol in traded_symbols:
-			  history = list(map(lambda x: x["close"], self.client.get_lastn_candle_history(symbol, 60, 10)))
-			  self.symbols.append(Symbol(symbol, np.array(history)))
+        for symbol in traded_symbols:
+            history = list(map(lambda x: x["close"], self.client.get_lastn_candle_history(symbol, 60, 10)))
+            self.symbols.append(Symbol(symbol, np.array(history)))
 
-	def trade(self):
-		step = 0
+    def trade(self):
+        step = 0
 
-		while True:
-			if(step == 0):
-				step = 6
+        while True:
+            if(step == 0):
+                step = 6
 
-			for symbol in self.symbols:
-				# skip closed markets
-				if trader.client.check_if_market_open([symbol.name])[symbol.name] == False:
-					continue
+                for symbol in self.symbols:
+                    # skip closed markets
+                    if trader.client.check_if_market_open([symbol.name])[symbol.name] == False:
+                        continue
 
-			    close = self.client.get_lastn_candle_history(symbol.name, 60, 1)[0]["close"]
-			    symbol.update_history(close)
-			    prediction = symbol.predict()
-			    print(f"{symbol.name}: {prediction}")
+                    close = self.client.get_lastn_candle_history(symbol.name, 60, 1)[0]["close"]
+                    symbol.update_history(close)
+                    prediction = symbol.predict()
+                    print(f"{symbol.name}: {prediction}")
 
-			    try:
-					if prediction == Prediction.BUY:
-						if symbol.status == TradeStatus.NOTHING:
-							"""
-							BUY LONG
-							"""
-							self.client.open_trade('buy', symbol.name, 0.8)
-							symbol.update_trade_status(TradeStatus.LONG)
-						elif symbol.status == TradeStatus.SHORT:
-							"""
-							SELL SHORT
-							"""
-							trades = trader.client.update_trades()
-							trade_id = list(filter(lambda r: r[0] == symbol.name, [(x[1].symbol, x[0]) for x in trades.items()]))[0][1]
-							self.client.close_trade(trade_id)
-							symbol.update_trade_status(TradeStatus.NOTHING)
-					elif prediction == Prediction.SELL:
-						if symbol.status == TradeStatus.NOTHING:
-							"""
-							BUY SHORT
-							"""
-							self.client.open_trade('sell', symbol.name, 0.8)
-							symbol.update_trade_status(TradeStatus.SHORT)
-						elif symbol.status == TradeStatus.LONG:
-							"""
-							SELL LONG
-							"""
-							trades = trader.client.update_trades()
-							trade_id = list(filter(lambda r: r[0] == symbol.name, [(x[1].symbol, x[0]) for x in trades.items()]))[0][1]
-							self.client.close_trade(trade_id)
-							symbol.update_trade_status(TradeStatus.NOTHING)
-				except Exception as e:
-				print(f"An exception occurred: {str(e)}")
-			else:
-				step -= 1
-				self.client.ping()
+                    try:
+                        if prediction == Prediction.BUY:
+                            if symbol.status == TradeStatus.NOTHING:
+                                """
+                                BUY LONG
+                                """
+                                self.client.open_trade('buy', symbol.name, 0.8)
+                                symbol.update_trade_status(TradeStatus.LONG)
+                            elif symbol.status == TradeStatus.SHORT:
+                                """
+                                SELL SHORT
+                                """
+                                trades = trader.client.update_trades()
+                                trade_id = list(filter(lambda r: r[0] == symbol.name, [(x[1].symbol, x[0]) for x in trades.items()]))[0][1]
+                                self.client.close_trade(trade_id)
+                                symbol.update_trade_status(TradeStatus.NOTHING)
+                        elif prediction == Prediction.SELL:
+                            if symbol.status == TradeStatus.NOTHING:
+                                """
+                                BUY SHORT
+                                """
+                                self.client.open_trade('sell', symbol.name, 0.8)
+                                symbol.update_trade_status(TradeStatus.SHORT)
+                            elif symbol.status == TradeStatus.LONG:
+                                """
+                                SELL LONG
+                                """
+                                trades = trader.client.update_trades()
+                                trade_id = list(filter(lambda r: r[0] == symbol.name, [(x[1].symbol, x[0]) for x in trades.items()]))[0][1]
+                                self.client.close_trade(trade_id)
+                                symbol.update_trade_status(TradeStatus.NOTHING)
+                    except Exception as e:
+                        print(f"An exception occurred: {str(e)}")
+                else:
+                    step -= 1
+                    self.client.ping()
 
-			time.sleep(10)
+        time.sleep(10)
 
 
 def clear_screen():
@@ -159,7 +159,7 @@ if __name__ == "__main__":
     Trader instance with trading loop
     TODO: treat login error 
     """
-    trader = Trader(symbols, cfg["ID"], password, cfg["mode"])
+    trader = Trader(traded_symbols, cfg["ID"], password, cfg["mode"])
     trader.trade()    
 
 
